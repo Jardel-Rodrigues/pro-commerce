25c23810694ae4a5bc4c9a4b29ccc51675935f9c
Jardel Rodrigues <rodriguinhosilvards96@gmail.com>
2024-10-26 19:32:57 -0300

    editado nome do repositorio

--- src/main/java/com/softstrem/ProCommerceApplication.java
+++ src/main/java/com/softstrem/ProCommerceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProCommercerApplication {
+public class ProCommerceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProCommercerApplication.class, args);
+		SpringApplication.run(ProCommerceApplication.class, args);
 	}
 
 }
