f6c58923d116216aa1d1f2ac963e16d3f258782f
Jardel Rodrigues <rodriguinhosilvards96@gmail.com>
2024-11-15 15:05:54 -0300

    Ajuste na query que busca o usuario.

--- src/main/java/com/softstrem/repositories/UserRepository.java
+++ src/main/java/com/softstrem/repositories/UserRepository.java
@@ -14,10 +14,10 @@ import com.softstrem.projections.UserDetailsProjection;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	@Query(nativeQuery = true, value = """
-			SELECT tb_user.email AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority
-			FROM tb_user
-			INNER JOIN tb_user_role ON tb_user.id = tb_user_role.user_id
-			INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id
+			SELECT pro_commerce.tb_user.email AS username, pro_commerce.tb_user.password, pro_commerce.tb_role.id AS roleId, pro_commerce.tb_role.authority
+			FROM pro_commerce.tb_user
+			INNER JOIN pro_commerce.tb_user_role ON pro_commerce.tb_user.id = pro_commerce.tb_user_role.user_id
+			INNER JOIN pro_commerce.tb_role ON pro_commerce.tb_role.id = pro_commerce.tb_user_role.role_id
 			WHERE tb_user.email = :email
 			""")
 	List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
